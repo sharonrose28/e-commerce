054c006d30e4f201c0b2c8fcad6f74533e11d41c
sharonrose28 <80213661+sharonrose28@users.noreply.github.com>
2023-11-22 18:47:25 +0530

    Update Products.jsx

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -20,7 +20,7 @@ const Products = () => {
     useEffect(() => {
         const getProducts = async () => {
             setLoading(true);
-            const response = await fetch("http://fakestoreapi.com/products");
+            const response = await fetch("https://fakestoreapi.com/products");
             if (componentMounted) {
                 setData(await response.clone().json());
                 setFilter(await response.json());
